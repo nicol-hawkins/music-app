6191dcc246b0db6b1e9bae0dc4b1a7de7b1cab73
nicol-hawkins <nicolchawkins@gmail.com>
2020-07-03 23:05:11 -0700

    fixed repeating DOM animations

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ window.addEventListener('load', () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener("animationend", function(){
+            visual.removeChild(this);
+        })
     }
 });
 
